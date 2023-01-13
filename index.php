9bbb2be891dd6619e8f572b4b6665479c3e987f6
PaoloWD <paolo.lore.93@gmail.com>
2023-01-13 17:00:07 +0100

    add second film with bonus

--- index.php
+++ index.php
@@ -2,13 +2,21 @@
 require_once "./classes/Movie.php";
 require_once "./classes/Genre.php";
 
-$genreMovie = new Genre(["Fantasy", "Action"]);
+$genreMovie_1 = new Genre(["Fantasy", "Action"]);
+$genreMovie_2 = new Genre(["Storia Vera", "Dramma"]);
 
 
-$firstMovie = new Movie("Pippo nel paese delle meraviglie", "ciaociao", "220", $genreMovie);
+$firstMovie = new Movie("Pippo nel paese delle meraviglie", "ciaociao", "220", $genreMovie_1);
+
+$secondMovie = new Movie("Paolo nel paese dei bonus", "Paolo parte sempre dai bonus per poi dimenticarsi di fare le milestone", "una vita", $genreMovie_2);
 
 echo '<pre>';
-echo '<h3>Film Completo</h3>'.'<br />';
+echo '<h3>Film Completo 1</h3>'.'<br />';
 print_r($firstMovie);
+echo '</pre>';
+
+echo '<pre>';
+echo '<h3>Film Completo 2</h3>'.'<br />';
+print_r($secondMovie);
 echo '</pre>';
 ?>
